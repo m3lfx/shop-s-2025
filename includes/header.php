bdf4a18c914d7c573689a712e30ed403843a841b
rommel dalisay <rommelmdalisay@gmail.com>
2025-10-03 08:36:46 +0800

    first commit

---
+++ includes/header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="includes/style/style.css" rel="stylesheet" type="text/css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <title>shop </title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">My Shop</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/itim211-shop-s">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <?php if (isset($_SESSION['user_id'])) {
+              echo '<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>';
+
+              echo '<ul class="dropdown-menu">';
+              if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') {
+                echo "<li><a class='dropdown-item' href='../item/index.php'>item</a></li>";
+                echo "<li><a class='dropdown-item' href='../admin/orders.php'>orders</a></li>";
+                echo "<li><a class='dropdown-item' href='../admin/users.php'>Users</a></li>";
+              } else {
+                echo '<li><a class="dropdown-item" href="./user/profile.php">profile</a></li>';
+                echo '<li><a class="dropdown-item" href="../user/myorders.php"> My Orders</a></li>';
+              }
+              echo "</ul>";
+            }
+            ?>
+
+
+          </li>
+
+        </ul>
+        <form action="search.php" method="GET" class="d-flex">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <?php
+        if (!isset($_SESSION['user_id'])) {
+          echo "<div class='navbar-nav ms-auto'>
+                        <a href='http://{$_SERVER['SERVER_NAME']}/shop-ns-2025/user/login.php'  class='nav-item nav-link'>Login</a></div>";
+        } else {
+          echo "<li class='nav-item'>
+           <p>{$_SESSION['email']}</p>
+          </li>";
+          echo "<div class='navbar-nav ms-auto'>
+                        <a href='http://{$_SERVER['SERVER_NAME']}/shop-ns-2025/user/logout.php'  class='nav-item nav-link'>Logout</a></div>";
+        }
+        ?>
+      </div>
+    </div>
+  </nav>
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+
+include('./includes/header.php');
+include('./includes/config.php');
+// unset($_SESSION['cart_products']);
+print_r($_SESSION);
+if(isset($_SESSION["cart_products"]) && count($_SESSION["cart_products"]) > 0) {
+    echo '<div class="cart-view-table-front" id="view-cart">';
+    echo '<h3>Your Shopping Cart</h3>';
+    echo '<form method="POST" action="cart_update.php">';
+    echo '<table width="100%"  cellpadding="6" cellspacing="0">';
+    echo '<tbody>';
+    $total = 0;
+    $b = 0;
+    foreach ($_SESSION["cart_products"] as $cart_itm) {
+        $product_name = $cart_itm["item_name"];
+        $product_qty = $cart_itm["item_qty"];
+        $product_price = $cart_itm["item_price"];
+        $product_code = $cart_itm["item_id"];
+        $bg_color = ($b++%2 == 1) ? 'odd' : 'even';
+        echo '<tr class="'.$bg_color.'">';
+        echo "<td>Qty <input type='number' size='2' maxlength='2' name='product_qty[$product_code]' value={$product_qty} /></td>";
+        echo "<td>{$product_name}</td>";
+        echo '<td><input type="checkbox" name="remove_code[]" value="'.$product_code.'" /> Remove</td>';
+        echo '</tr>';
+        $subtotal = ($product_price * $product_qty);
+        $total += $subtotal;
+    }
+    echo '<td colspan="4">';
+    echo '<button type="submit">Update</button><a href="view_cart.php" class="button">Checkout</a>';
+    echo '</td>';
+    echo '</tbody>';
+    echo '</table>';
+    echo "</form>";
+    echo '</div>';
+}
+$sql = "SELECT i.item_id AS itemId, description, img_path, sell_price FROM item i INNER JOIN stock s USING (item_id)  ORDER BY i.item_id ASC";
+
+$results = mysqli_query($conn, $sql);
+
+if ($results) {
+    $products_item = '<ul class="products">';
+
+    //fetch results set as object and output HTML
+    while ($row = mysqli_fetch_assoc($results)) {
+        $products_item .= <<<EOT
+     <li class="product">
+    <form method="POST" action="cart_update.php">
+    <div class="product-content"><h3>{$row['description']}</h3>
+    <div class="product-thumb"><img src="./item/{$row['img_path']}" width="50px" height="50px"></div>
+    <div class="product-info">
+    Price {$row['sell_price']} 
+    <fieldset>
+    
+    <label>
+        <span>Quantity</span>
+        <input type="number" size="2" maxlength="2" name="item_qty" value="1" />
+    </label>
+    </fieldset>
+    <input type="hidden" name="item_id" value="{$row['itemId']}" />
+    <input type="hidden" name="type" value="add" />
+    
+    <div align="center"><button type="submit" class="add_to_cart">Add</button></div>
+    </div></div>
+    </form>
+    </li>
+EOT;
+    }
+
+    $products_item .= '</ul>';
+    echo $products_item;
+}
+include('./includes/footer.php');
+
